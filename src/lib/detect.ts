f6a7c5b779cade956daafc7b99ece6ef7a61afa1
zhangpan <zhangpanrobot@gmail.com>
2024-01-13 23:40:00 +0800

    detect class refine

--- src/lib/detect.ts
+++ src/lib/detect.ts
@@ -1,3 +1,6 @@
+import Toastify from 'toastify-js'
+import { initEventHandler, getActiveTab } from '@/utils/extension-action'
+
 const DETECT_CLASS = 'sss-detect'
 const PREVIEW_CLASS = 'sss-preview'
 enum DetectState {
@@ -8,30 +11,49 @@ enum DetectState {
 
 export class Detect {
   detectState: DetectState = DetectState.Init
-  target: HTMLElement | null = null
+  targetEl: HTMLElement | null = null
   handleOver = () => this.detectingAction(true)
   handleOut = () => this.detectingAction(false)
   handleConfirm = () => this.toConfirm
-  constructor() {
-    this.init()
-  }
   init() {
+    initEventHandler({
+      'to-detect': this.toDetect,
+      'to-preview': this.preview,
+      confirmed: this.reset,
+    })
+    this.reset()
+  }
+  reset() {
     this.detectState = DetectState.Init
-    this.target = null
+    this.targetEl = null
+    this.eventCancel()
   }
   toDetect() {
-    this.detectState = DetectState.Detecting
     this.eventInit()
+    this.detectState = DetectState.Detecting
   }
   toCancel() {
-    this.target && this.target.classList.remove(DETECT_CLASS)
-    this.init()
-    this.eventCancel()
-    console.log('cancel')
+    this.targetEl && this.targetEl.classList.remove(DETECT_CLASS)
+    this.reset()
   }
-  toPreview() {
-    if (!this.target || !this.target.classList?.contains(DETECT_CLASS)) return
-    this.target.classList.add(PREVIEW_CLASS)
+  preview(selector: string | undefined) {
+    if (selector) {
+      try {
+        this.targetEl = document.querySelector(selector)
+      } catch (e: any) {
+        Toastify({
+          text: e.message,
+        }).showToast()
+        return
+      }
+    }
+    if (!this.targetEl || !this.targetEl.classList?.contains(DETECT_CLASS)) {
+      Toastify({
+        text: 'No preview Element found, please check your Selector',
+      }).showToast()
+      return
+    }
+    this.targetEl.classList.add(PREVIEW_CLASS)
   }
   eventInit() {
     const body = document.body
@@ -46,15 +68,20 @@ export class Detect {
     body.removeEventListener('click', this.handleConfirm)
   }
   toConfirm() {
-    if (!this.target || !this.target.classList?.contains(DETECT_CLASS)) return
-    openWindow(this.target)
+    if (!this.targetEl || !this.targetEl.classList?.contains(DETECT_CLASS)) return
+    chrome.runtime.sendMessage({
+      greeting: 'to-open-window',
+      data: {
+        selector: getSelectorByEl(this.targetEl),
+      },
+    })
     this.detectState = DetectState.Detected
   }
   detectingAction(isHover: boolean) {
     return (event: MouseEvent) => {
       if (this.detectState === 2) return
       const target = event.target as HTMLElement
-      this.target = target
+      this.targetEl = target
       const classList = target.classList
       if (isHover) {
         classList.add(DETECT_CLASS)
@@ -65,13 +92,122 @@ export class Detect {
   }
 }
 
-function openWindow(target: HTMLElement) {
-  chrome.runtime
-    .sendMessage({
-      greeting: 'to-open-window',
-      data: 111,
+export class DetectWindow {
+  tabId: number = 0
+  selector: string = ''
+  cancelEle: HTMLElement | null = null
+  previewEle: HTMLElement | null = null
+  unselectEle: HTMLElement | null = null
+  constructor() {
+    this.init()
+  }
+  init() {
+    initEventHandler({
+      'to-detect': (data: { tabId: number; selector: string }) => {
+        const { tabId, selector } = data
+        this.tabId = tabId
+        this.selector = selector
+      },
     })
-    .then(() => {
-      console.log(target, 'openwindow')
+    this.cancelEle = document.getElementById('cancel')
+    this.previewEle = document.getElementById('preview')
+    this.unselectEle = document.getElementById('unselect')
+    this.cancelEle?.addEventListener('click', () => {
+      // close window
+      getActiveTab().then(({ windowId }) => chrome.windows.remove(windowId))
     })
+
+    this.unselectEle?.addEventListener('click', () => {
+      console.log('unselect')
+    })
+    if (!this.previewEle) return
+    this.previewEle.addEventListener('click', (e: MouseEvent) => {
+      const targetEl = e.target as HTMLElement
+      const previewData = targetEl.getAttribute('data-preview')
+      if (previewData === 'inactive') {
+        targetEl.textContent = 'Exit Preview'
+        targetEl.setAttribute('data-preview', 'active')
+        chrome.tabs.sendMessage(this.tabId, { greeting: 'to-preview' })
+      } else {
+        targetEl.textContent = 'Preview'
+        targetEl.setAttribute('data-preview', 'inactive')
+        chrome.tabs.sendMessage(this.tabId, { greeting: 'to-exit-preview' })
+      }
+    })
+  }
+  preview() {
+    chrome.tabs.sendMessage(this.tabId, {
+      greeting: 'to-preview',
+    })
+  }
+  confirm() {
+    chrome.runtime.sendMessage({
+      greeting: 'detect-confirm',
+    })
+  }
+}
+
+export class DetectService {
+  tabId: number = 0
+  windowId: number = 0
+  constructor(tabId: number) {
+    this.init(tabId)
+  }
+  init(tabId: number) {
+    this.tabId = tabId
+    initEventHandler({
+      confirm: this.confirm,
+      'to-detect': this.preDetect,
+    })
+  }
+  destroy() {
+    this.tabId = 0
+  }
+  confirm(selector: string) {
+    // TODO: get domain and path
+    chrome.storage.sync.set({ domain_path: selector })
+  }
+  openWindow(selector: string) {
+    return chrome.windows
+      .create({
+        url: 'src/window/index.html',
+        type: 'popup',
+        width: 600,
+        height: 300,
+        focused: true,
+        left: 0,
+        top: 0,
+      })
+      .then((windowInfo) => {
+        const { tabs } = windowInfo
+        if (!tabs?.length || !tabs[0].id) return
+        chrome.tabs.sendMessage(tabs[0].id, {
+          greeting: 'to-detect',
+          data: {
+            selector,
+            tabId: this.tabId,
+          },
+        })
+      })
+  }
+  closeWindow() {
+    chrome.windows.remove(this.windowId)
+  }
+  preDetect() {
+    if (this.windowId !== 0) this.closeWindow()
+    getActiveTab({ currentWindow: false }).then((tabId) => {
+      this.tabId = tabId
+      chrome.tabs.sendMessage(tabId, {
+        greeting: '',
+      })
+    })
+  }
+  postDetect(selector: string) {
+    this.openWindow(selector)
+  }
+}
+
+// TODO: get select by element
+function getSelectorByEl(element: HTMLElement): string {
+  return element.className
 }
--- src/window/index.ts
+++ src/window/index.ts
@@ -1,30 +1,4 @@
 import './index.scss'
-import { getActiveTab } from '@/utils/extension-action'
+import { DetectWindow } from '@/lib/detect'
 
-const cancelEle = document.getElementById('cancel')
-const previewEle = document.getElementById('preview')
-const unselectEle = document.getElementById('unselect')
-
-cancelEle?.addEventListener('click', () => {
-  // close window
-  getActiveTab().then(({ windowId }) => chrome.windows.remove(windowId))
-})
-
-unselectEle?.addEventListener('click', () => {
-  console.log('unselect')
-})
-
-previewEle?.addEventListener('click', () => {
-  const previewData = previewEle.getAttribute('data-preview')
-  if (previewData === 'inactive') {
-    previewEle.textContent = 'Exit Preview'
-    previewEle.setAttribute('data-preview', 'active')
-    getActiveTab({ currentWindow: false }).then(({ id }) => chrome.tabs.sendMessage(id, { greeting: 'to-preview' }))
-  } else {
-    previewEle.textContent = 'Preview'
-    previewEle.setAttribute('data-preview', 'inactive')
-    getActiveTab({ currentWindow: false }).then(({ id }) =>
-      chrome.tabs.sendMessage(id, { greeting: 'to-exit-preview' }),
-    )
-  }
-})
+new DetectWindow()
